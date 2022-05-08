11a1b1b0434cafabce2b6e0ca308018ff64e975a
Wállaci Aparecido <wallaci.contato@gmail.com>
2022-05-08 15:18:08 -0300

    Finalizando

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      colors:{
+        brand:{
+          300: '#589fda',
+          500: '#2c82c9',
+          600: '#2c62c9',
+        }
+      },
+      borderRadius:{
+        md: '4px',
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar'),
+    // ...
+  ],
+}
